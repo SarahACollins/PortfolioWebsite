5da13e7499869096f179aeee9cc5f47e202d8106
SarahACollins <saraha_collins@outlook.com>
2021-10-09 20:09:32 -0700

    Initial commit

---
+++ build/precache-manifest.d8f2b3cb75a42422bdd69ff38a631bdc.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "558cc39197db4bdeed029fb600114025",
+    "url": "/PortfolioWebsite/index.html"
+  },
+  {
+    "revision": "02a2c77b96ae1754db22",
+    "url": "/PortfolioWebsite/static/css/main.5dc4aae4.chunk.css"
+  },
+  {
+    "revision": "b5800831f7bbffdb4646",
+    "url": "/PortfolioWebsite/static/js/2.22f42dc4.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/PortfolioWebsite/static/js/2.22f42dc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02a2c77b96ae1754db22",
+    "url": "/PortfolioWebsite/static/js/main.3dab30ef.chunk.js"
+  },
+  {
+    "revision": "54fce115b5c555b51a16",
+    "url": "/PortfolioWebsite/static/js/runtime-main.ecf639cc.js"
+  },
+  {
+    "revision": "83f7df01a8a9b0ab70d88d523679ab18",
+    "url": "/PortfolioWebsite/static/media/apothecaryimage.83f7df01.jpeg"
+  },
+  {
+    "revision": "42ebc5dedc0f1a19a61361000ffeb611",
+    "url": "/PortfolioWebsite/static/media/barchart1.42ebc5de.png"
+  },
+  {
+    "revision": "dddd12edfc727b126e2d1bb7b11a22cd",
+    "url": "/PortfolioWebsite/static/media/barchart1alt.dddd12ed.png"
+  },
+  {
+    "revision": "c490a14bc9af4ed94225b76cf7a90c6b",
+    "url": "/PortfolioWebsite/static/media/barchart2.c490a14b.png"
+  },
+  {
+    "revision": "1edd7b20708af374ed08a12ba1e85110",
+    "url": "/PortfolioWebsite/static/media/barchart2alt.1edd7b20.png"
+  },
+  {
+    "revision": "2b6275550c9cc2cb3168f0fc294e228b",
+    "url": "/PortfolioWebsite/static/media/githubicon.2b627555.png"
+  },
+  {
+    "revision": "0fa7809726cda798738a2fc65e6b9323",
+    "url": "/PortfolioWebsite/static/media/headshot.0fa78097.png"
+  },
+  {
+    "revision": "19fc2a83fc1f2333b95d3479f961df45",
+    "url": "/PortfolioWebsite/static/media/headshot3.19fc2a83.png"
+  },
+  {
+    "revision": "f4b6e163c5b20c7132c8e2091a1491ef",
+    "url": "/PortfolioWebsite/static/media/hotspot.f4b6e163.gif"
+  },
+  {
+    "revision": "7e624733380755e38dadefc3eb83e5c0",
+    "url": "/PortfolioWebsite/static/media/largelunarlogo.7e624733.PNG"
+  },
+  {
+    "revision": "8f9e38c64bae07e2b8c42292d6b3dec5",
+    "url": "/PortfolioWebsite/static/media/lunarlogo.8f9e38c6.png"
+  },
+  {
+    "revision": "46fc45e88da7ffba9ae559ad251344a2",
+    "url": "/PortfolioWebsite/static/media/map.46fc45e8.gif"
+  },
+  {
+    "revision": "19c1174c03ced98b2e52eb5cc892254d",
+    "url": "/PortfolioWebsite/static/media/practicemode.19c1174c.gif"
+  },
+  {
+    "revision": "c38df483a96f6d0133ee0cff08efceed",
+    "url": "/PortfolioWebsite/static/media/unity-mwu-white.c38df483.png"
+  },
+  {
+    "revision": "b994b4a899cac186e2d0747de9587893",
+    "url": "/PortfolioWebsite/static/media/userinfomode.b994b4a8.gif"
+  }
+]);
